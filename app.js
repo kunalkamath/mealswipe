ec310ff8ba36546c1c5355d7f1f4004bfcd44b63
Esha Maharishi <em2852@columbia.edu>
2015-04-11 14:47:39 -0400

    cleaning up line spacing

--- app.js
+++ app.js
@@ -16,20 +16,17 @@ var server = app.listen(3000, function () {
 
 app.get('/', function (req, res) {
 
-  var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
+  var MongoClient = require('mongodb').MongoClient,
+      format = require('util').format;
 
   MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
     if(err) throw err;
-
     var collection = db.collection('test_insert');
     collection.insert({a:2}, function(err, docs) {
-
       collection.count(function(err, count) {
         console.log(format("count = %s", count));
         res.send(format("count = %s", count));
       });
-
       collection.find().toArray(function(err, results) {
         console.dir(results);
         db.close();
