618695359d268c38d9d650b2679f9a3bd294f6b0
Esha Maharishi <em2852@columbia.edu>
2015-04-11 14:44:28 -0400

    switching over to cleaner structure

---
+++ app.js
@@ -0,0 +1,48 @@
+// import
+var express = require('express'),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    CollectionDriver = require('./collectionDriver').CollectionDriver;
+
+var app = express();
+var mongoHost = 'localhost';
+var mongoPort = 27017;
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(server.address())
+  console.log('Express server listening on port: %s', host, port);
+});
+
+app.get('/', function (req, res) {
+
+  var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+  MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+
+      collection.count(function(err, count) {
+        console.log(format("count = %s", count));
+        res.send(format("count = %s", count));
+      });
+
+      collection.find().toArray(function(err, results) {
+        console.dir(results);
+        db.close();
+      });
+    });
+  });
+});
+
+app.get('/:active', function(req, res) {
+  res.send('Heorld!');
+  console.log("hi");
+});
+
+
+
