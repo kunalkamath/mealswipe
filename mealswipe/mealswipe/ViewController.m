ec9bd4477933b4ff6f342d4c78aac95916576126
Esha Maharishi <em2852@columbia.edu>
2015-04-12 00:59:39 -0400

    adding ios code

--- mealswipe/mealswipe/ViewController.m
+++ mealswipe/mealswipe/ViewController.m
@@ -8,15 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController()
 @end
 
 @implementation ViewController
 
+
+@synthesize nameLabel, phoneLabel, emailLabel, nameText, phoneText, emailText;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"in view did load");
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"i'm in view will appear");
+    NSLog(@"Hi");
+}
+
+- (IBAction)button:(id)sender {
+    NSLog(@"button pressed");
+
+    NSMutableDictionary *mutableDictionary = [[NSMutableDictionary alloc] init];
+    
+    NSString *name = nameText.text;
+    NSString *phone = phoneText.text;
+    NSString *email = emailText.text;
+    
+    NSMutableString *urls = [[NSMutableString alloc]init];
+    [urls appendString:@"http://localhost:3000/register/:"];
+    [urls appendString:name];
+    [urls appendString:@"/:"];
+    [urls appendString:phone];
+    [urls appendString:@"/:"];
+    [urls appendString:email];
+    NSURL* url = [NSURL URLWithString:urls];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    
+    NSData* data = [NSJSONSerialization dataWithJSONObject:mutableDictionary options:0 error:NULL];
+    request.HTTPBody = data;
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!error) {
+            NSLog(@"Sent new user");
+        }
+        else {
+            NSLog(@"error in nsurl sending");
+            NSLog(@"%@",[error localizedDescription]);
+        }
+    }];
+    [dataTask resume];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +74,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)register:(id)sender {
+}
 @end
--- mealswipe/mealswipe/ViewController2.m
+++ mealswipe/mealswipe/ViewController2.m
@@ -16,7 +16,71 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _scoreLabel = [ [UILabel alloc ] initWithFrame:CGRectMake((self.view.bounds.size.width / 2), 0.0, 150.0, 43.0) ];
+    _scoreLabel.textColor = [UIColor whiteColor];
+    _scoreLabel.backgroundColor = [UIColor redColor];
+    _scoreLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:(36.0)];
+    [self.view addSubview:_scoreLabel];
+    _scoreLabel.text = [NSString stringWithFormat: @"%d", 0];
     // Do any additional setup after loading the view.
+
+    NSMutableDictionary *mutableDictionary = [[NSMutableDictionary alloc] init];
+    
+    NSMutableString *urls = [[NSMutableString alloc]init];
+    [urls appendString:@"http://localhost:3000/active"];
+    NSURL* url = [NSURL URLWithString:urls];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"GET";
+    
+    NSData* data = [NSJSONSerialization dataWithJSONObject:mutableDictionary options:0 error:NULL];
+    request.HTTPBody = data;
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            NSLog(@"error in nsurl sending");
+            NSLog(@"%@",[error localizedDescription]);
+        }
+        else {
+
+            NSLog(@"Sent new user");
+            NSArray* responseArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+            NSLog(@"array: %@", responseArray);
+            
+            NSMutableArray *mainArray = [NSMutableArray array];
+            
+            /*
+            for(int i = 0; i < 5; i++)
+            {
+                NSMutableArray *subArray = [[NSMutableArray alloc] initWithCapacity:5];
+                
+                for(int j = 0; j < 4; j++)
+                {
+                    UILabel *label = [[UILabel alloc] init];
+                initWithFrame:CGRectMake((self.view.bounds.size.width / 2), 0.0, 150.0, 43.0) ];
+                    _scoreLabel.textAlignment =  UITextAlignmentCenter;
+                    _scoreLabel.textColor = [UIColor whiteColor];
+                    _scoreLabel.backgroundColor = [UIColor redColor];
+                    _scoreLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:(36.0)];
+                    [self.view addSubview:_scoreLabel];
+                    _scoreLabel.text = [NSString stringWithFormat: @"%d", 0];
+                    [subArray addObject:label];
+                    [label release];
+                }
+                [mainArray addObject:subArray];
+                [subArray release];
+            }
+            
+             */
+        }
+
+    }];
+    [dataTask resume];
 }
 
 - (void)didReceiveMemoryWarning {
